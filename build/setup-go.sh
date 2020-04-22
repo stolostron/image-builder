e2ad0d768f17f4d3c51cbf2d1192279f6394a67b
Ray Harris <raharris@redhat.com>
2020-04-22 09:16:33 -0400

    factor out build steps into common script

---
+++ build/setup-go.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# Install dependencies from yum
+yum install -y \
+    bc \
+    file \
+    findutils \
+    gcc \
+    gpgme \
+    git \
+    hostname \
+    lsof \
+    make \
+    openssl \
+    openssl-devel \
+    socat \
+    systemd-devel \
+    tar \
+    util-linux \
+    wget \
+    which \
+    zip
+yum clean all
+
+# Install tree command
+mkdir -p /opt/tree
+cd /opt/tree
+wget http://mama.indstate.edu/users/ice/tree/src/tree-1.8.0.tgz
+tar xf tree-1.8.0.tgz
+cd /opt/tree/tree-1.8.0
+make
+make install
+rm -rf /opt/tree
+
+# Install go
+mkdir -p /go/{bin,pkg,src} /opt/go
+cd /opt/go
+wget --progress=dot:mega https://dl.google.com/go/go${GOVERSION}.${GOOS}-${GOARCH}.tar.gz
+tar -C /usr/local -xzf go${GOVERSION}.${GOOS}-${GOARCH}.tar.gz
+rm -rf /opt/go
+
+# Install build harness
+mkdir -p /build-harness
+cd /build-harness
+git clone https://github.com/cloudposse/build-harness.git
+git clone https://github.com/open-cluster-management/build-harness-extensions.git
+rm -rf build-harness/.git
+rm -rf build-harness-extensions/.git
