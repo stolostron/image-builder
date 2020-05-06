ebc6ce821e6ccc0ffbbfbb11dedbbbac40c3532f
Ray Harris <raharris@redhat.com>
2020-05-06 11:10:51 -0400

    add kubebuilder (#7)
    * split setup into multiple files
    
    * refactor setup and add kubebuilder

---
+++ build/setup-kubebuilder.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Install kubebuilder
+export KBVERSION=2.2.0
+mkdir -p /opt/kubebuilder
+cd /opt/kubebuilder
+wget -O kubebuilder.tgz --progress=dot:mega https://go.kubebuilder.io/dl/$KBVERSION/$GOOS/$GOARCH
+tar xf kubebuilder.tgz
+mv kubebuilder_${KBVERSION}_${GOOS}_${GOARCH} /usr/local/kubebuilder
+cd /
+rm -rf /opt/kubebuilder
---
+++ build/setup-sonar.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Install dependencies from yum
+yum install -y \
+    java-11-openjdk-headless \
+    zip
+yum clean all
+
+# Install sonar scanner
+cd /opt
+wget --progress=dot:mega https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-4.2.0.1873-linux.zip
+unzip sonar-scanner-cli-4.2.0.1873-linux.zip
+rm sonar-scanner-cli-4.2.0.1873-linux.zip
+mv sonar-scanner-4.2.0.1873-linux sonar
