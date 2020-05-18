9a83e29d07c6ea6fa2b0bb307baf763b51472df0
Ray Harris <raharris@redhat.com>
2020-05-18 13:33:08 -0400

    Update setup-jq.sh (#17)

--- build/setup-jq.sh
+++ build/setup-jq.sh
@@ -5,5 +5,5 @@ mkdir -p /opt/jq
 cd /opt/jq
 wget https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64
 chmod +x jq-linux64
-mv jq-linux64 /usr/local/bin
+mv jq-linux64 /usr/local/bin/jq
 rm -rf /opt/jq
