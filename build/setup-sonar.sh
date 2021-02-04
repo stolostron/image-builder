7188ca4f5488ccc706ca36fe0fd63afa8b8c43a0
Ray Harris <raharris@redhat.com>
2021-02-04 09:48:01 -0500

    sonar: add SONAR_USER_HOME

--- build/setup-sonar.sh
+++ build/setup-sonar.sh
@@ -12,3 +12,7 @@ wget --progress=dot:mega https://binaries.sonarsource.com/Distribution/sonar-sca
 unzip sonar-scanner-cli-4.2.0.1873-linux.zip
 rm sonar-scanner-cli-4.2.0.1873-linux.zip
 mv sonar-scanner-4.2.0.1873-linux sonar
+
+# Create Sonar Home
+mkdir /opt/sonar/.sonar
+chmod 777 /opt/sonar/.sonar
