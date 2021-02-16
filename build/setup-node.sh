d3e60a4802f1c4e5860e1c631856c6071517be57
Andrew McNamara <arewm@users.noreply.github.com>
2021-02-16 14:24:49 -0500

    Add yarn to nodejs builders (#59)
    * add yarn build script
    
    Signed-off-by: arewm <arewm@users.noreply.github.com>
    
    * update image builds
    
    Signed-off-by: arewm <arewm@users.noreply.github.com>

--- build/setup-node.sh
+++ build/setup-node.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-# Setup Node RPM Repository
-curl -sL https://rpm.nodesource.com/setup_$NODE_VERSION.x | bash -
-
 # Install Node and npm
-yum install -y nodejs
+dnf module -y enable nodejs:$NODE_VERSION && dnf install -y nodejs
 
-yum clean all
+dnf clean all
---
+++ build/setup-yarn.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+package_location=$1
+# Install yarn
+cd $package_location
+npm install
