0a78492499cc0d2bd412703028be7029e8ae2d48
Ray Harris <raharris@redhat.com>
2021-03-08 11:10:19 -0500

    node: set node cache directory (#69)

--- build/setup-node.sh
+++ build/setup-node.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
+# Create node cache directory
+mkdir -p /opt/node/cache
+chmod -r a+rwx /opt/node
+
 # Install Node and npm
 dnf module -y enable nodejs:$NODE_VERSION && dnf install -y nodejs
+
+# Set node cache director
+npm config set cache /opt/node/cache --global
 
 dnf clean all
