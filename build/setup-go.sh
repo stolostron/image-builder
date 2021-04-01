38cd40c1a694d236f0c06253d3b682998aa31c93
Ray Harris <raharris@redhat.com>
2021-04-01 12:07:16 -0400

    unify s390x and amd64 Dockerfiles

--- build/setup-go.sh
+++ build/setup-go.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
 # Install go
+OS=$(uname -s | tr '[:upper:]' '[:lower:]')
+ARCH=$(uname -m)
+if [[ "$ARCH" == "x86_64" ]]; then
+    ARCH="amd64"
+fi
+
 mkdir -p /go/{bin,pkg,src} /go/pkg/cache /opt/go
 chmod -R a+rwx /go
 cd /opt/go
-wget --progress=dot:mega https://dl.google.com/go/go${GOVERSION}.${GOOS}-${GOARCH}.tar.gz
-tar -C /usr/local -xzf go${GOVERSION}.${GOOS}-${GOARCH}.tar.gz
+wget --progress=dot:mega https://dl.google.com/go/go${GOVERSION}.${OS}-${ARCH}.tar.gz
+tar -C /usr/local -xzf go${GOVERSION}.${OS}-${ARCH}.tar.gz
 rm -rf /opt/go
-if [ "$(uname -m)" = "s390x" ]; then
+if [ "$ARCH" = "s390x" ]; then
         cd /usr/bin
         ln gcc s390x-linux-gnu-gcc
 fi
