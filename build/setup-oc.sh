7392c9950e4e8dc396783ccb1cc91cd341ab70c8
Raja <68847302+raja-sekhar-r@users.noreply.github.com>
2021-04-01 14:00:19 +0530

    s390x changes

--- build/setup-oc.sh
+++ build/setup-oc.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 # Install kubectl and oc
-url="https://mirror.openshift.com/pub/openshift-v4/clients/ocp/latest/openshift-client-linux.tar.gz"
+
+if [ "$(uname -m)" = "s390x" ]; then
+        url="http://mirror.openshift.com/pub/openshift-v4/s390x/clients/ocp/latest/openshift-client-linux.tar.gz"
+elif [ "$(uname -m)" = "x86_64" ];  then
+        url="https://mirror.openshift.com/pub/openshift-v4/clients/ocp/latest/openshift-client-linux.tar.gz"
+fi
 cd /usr/local/bin
 curl -sL "$url" | tar -zx
