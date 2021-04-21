4f212514873f299fe43493324793bb1a08307d8a
Ray Harris <raharris@redhat.com>
2021-04-21 11:56:37 -0400

    kind: update terraform files

---
+++ kind/variables.tf
@@ -0,0 +1,21 @@
+# Required variables
+variable "aws_secret" {
+  type = string
+}
+
+# Optional variables
+variable "aws_instance_type" {
+  type = string
+  default = "t2.2xlarge"
+}
+
+variable "aws_region" {
+  type = string
+  default = "us-east-1"
+}
+
+# public_key is required for the create step
+variable "public_key" {
+  type = string
+  default = ""
+}
