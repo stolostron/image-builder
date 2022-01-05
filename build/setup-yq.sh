e190d54d2f167aef2ba3bb2c082fdc7c2cd08682
Ray Harris <raharris@redhat.com>
2022-01-05 16:04:37 -0500

    add yq and update go version (#103)

---
+++ build/setup-yq.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+# Install yq command
+VERSION="v4.16.2"
+ARCH=$(uname -m)
+if [[ "$ARCH" == "x86_64" ]]; then
+    ARCH=amd64
+fi
+
+BINARY="yq_linux_$ARCH"
+
+wget "https://github.com/mikefarah/yq/releases/download/${VERSION}/${BINARY}" -O /usr/bin/yq
+chmod +x /usr/bin/yq
