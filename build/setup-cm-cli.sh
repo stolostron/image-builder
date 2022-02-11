cae05849b7b4a9fc7653aa2e40e1666bafcc4206
David Schmidt <51931019+schmidtd@users.noreply.github.com>
2022-02-11 11:52:58 -0500

    Update go versions to 1.17.7 and 1.16.14 (#107)
    * Update go versions to 1.17.7 and 1.16.14
    
    Signed-off-by: David Schmidt <51931019+schmidtd@users.noreply.github.com>
    
    * Download cm-cli instead of building it because go
    
    * Don't forget nodejs14
    
    Signed-off-by: David Schmidt <51931019+schmidtd@users.noreply.github.com>
    
    * Tidy up a smidge

--- build/setup-cm-cli.sh
+++ build/setup-cm-cli.sh
@@ -2,11 +2,20 @@
 
 set -e
 
-# Install cm-cli plugin
-cd /opt
-git clone https://github.com/stolostron/cm-cli.git
-cd cm-cli
-make plugin
-cd /
-rm -rf /opt/cm-cli
-chmod -R a+rwx /go
+# Download cm-cli
+OS=$(uname -s | tr '[:upper:]' '[:lower:]')
+ARCH=$(uname -m)
+if [[ "$ARCH" == "x86_64" ]]; then
+    ARCH="amd64"
+fi
+
+wget --progress=dot:mega https://github.com/stolostron/cm-cli/releases/download/${CM_VERSION}/cm_${OS}_${ARCH}.tar.gz
+tar -C /tmp -xzf cm_${OS}_${ARCH}.tar.gz
+
+# Spread cm-cli plugin around
+cp /tmp/cm ${GOPATH}/bin/oc-cm
+mv /tmp/cm ${GOPATH}/bin/kubectl-cm
+chmod -R a+rwx ${GOPATH}
+
+# Tidy up
+rm cm_${OS}_${ARCH}.tar.gz
