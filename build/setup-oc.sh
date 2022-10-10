7ef7f7ef49553395d01a9c40a476a1dca822644f
Gurney Buchanan <gurney.buchanan@gmail.com>
2022-10-10 15:20:27 -0400

    Genericize curl fo oc

--- build/setup-oc.sh
+++ build/setup-oc.sh
@@ -3,13 +3,6 @@
 set -e
 
 # Install kubectl and oc
-
-if [ "$(uname -m)" = "s390x" ]; then
-        url="http://mirror.openshift.com/pub/openshift-v4/s390x/clients/ocp/latest/openshift-client-linux.tar.gz"
-elif [ "$(uname -m)" = "x86_64" ];  then
-        url="https://mirror.openshift.com/pub/openshift-v4/clients/ocp/latest/openshift-client-linux.tar.gz"
-elif [ "$(uname -m)" = "aarch64" ];  then
-        url="https://mirror.openshift.com/pub/openshift-v4/aarch64/clients/ocp/latest/openshift-client-linux.tar.gz"
-fi
+url="https://mirror.openshift.com/pub/openshift-v4/$(uname -m)/clients/ocp/latest/openshift-client-linux.tar.gz"
 cd /usr/local/bin
 curl -sL "$url" | tar -zx
