15ec6e139e1735de436155538b40505400758d0f
Dale Haiducek <19750917+dhaiducek@users.noreply.github.com>
2025-09-19 10:34:50 -0400

    Update install scripts
    - Bump version to latest
    - Use latest version for `jq`, `yq`, `cm-cli`
    - Fix terraform script
    - Address Shellcheck
    - Use ${} variable syntax (because I like it :smile:)
    
    Signed-off-by: Dale Haiducek <19750917+dhaiducek@users.noreply.github.com>

--- build/setup-sonar.sh
+++ build/setup-sonar.sh
@@ -2,10 +2,11 @@
 
 set -e
 
-version="5.0.1.3006"
-sonar_file="sonar-scanner-cli-${version}-linux.zip"
-sonar_dir="sonar-scanner-${version}-linux"
-sonar_url="https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/$sonar_file"
+SONAR_VERSION="7.2.0.5079"
+
+sonar_file="sonar-scanner-cli-${SONAR_VERSION}-linux-x64.zip"
+sonar_dir="sonar-scanner-${SONAR_VERSION}-linux-x64"
+sonar_url="https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/${sonar_file}"
 
 # Install dependencies from yum
 yum install -y \
@@ -15,10 +16,10 @@ yum clean all
 
 # Install sonar scanner
 cd /opt
-wget --progress=dot:mega "$sonar_url"
-unzip "$sonar_file"
-rm "$sonar_file"
-mv "$sonar_dir" sonar
+wget --progress=dot:mega "${sonar_url}"
+unzip "${sonar_file}"
+rm "${sonar_file}"
+mv "${sonar_dir}" sonar
 
 # Create Sonar Home
 mkdir /opt/sonar/.sonar
--- build/setup-terraform.sh
+++ build/setup-terraform.sh
@@ -6,16 +6,16 @@ if [[ "$(uname -m)" == "aarch64" ]]; then
     # Necessary workaround on ARM until this issue is resolved: https://github.com/hashicorp/terraform/issues/32008
 
     # Pull the latest terraform version from the file list at https://releases.hashicorp.com/terraform by selecting all versions, sorting, and selecting the latest/last.   
-    LATEST_TF_VERSION=$(curl -X GET "https://releases.hashicorp.com/terraform" | sed -rn "s/.*terraform_([0-9]+\.[0-9]+\.[0-9]+)<.*/\1/p" | sort | tail -n 1)
+    LATEST_TF_VERSION=$(curl -X GET "https://releases.hashicorp.com/terraform" | sed -rn "s/.*terraform_([0-9]+\.[0-9]+\.[0-9]+)<.*/\1/p" | sort --version-sort | tail -n 1)
 
     # Pull and extract terraform at the target version in /usr/local/bin
     cd /usr/local/bin
     wget "https://releases.hashicorp.com/terraform/${LATEST_TF_VERSION}/terraform_${LATEST_TF_VERSION}_linux_arm64.zip"
-    unzip terraform_${LATEST_TF_VERSION}_linux_arm64.zip
+    unzip "terraform_${LATEST_TF_VERSION}_linux_arm64.zip"
     chmod +x terraform
-    rm -f terraform_${LATEST_TF_VERSION}_linux_arm64.zip
+    rm -f "terraform_${LATEST_TF_VERSION}_linux_arm64.zip"
 else
-    # Folowing the documentation for RHEL/CentOS here - https://www.terraform.io/downloads
+    # Following the documentation for RHEL/CentOS here - https://www.terraform.io/downloads
 
     # Add HashiCorp repo
     yum config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
