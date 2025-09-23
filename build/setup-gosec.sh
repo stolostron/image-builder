50433f3adccc5c6e612af494dd605fd3d65d92db
Dale Haiducek <19750917+dhaiducek@users.noreply.github.com>
2025-09-23 11:39:56 -0400

    Pivot to `go install`
    Signed-off-by: Dale Haiducek <19750917+dhaiducek@users.noreply.github.com>

--- build/setup-gosec.sh
+++ build/setup-gosec.sh
@@ -4,16 +4,9 @@ set -e
 
 # Install gosec
 if [[ "$(uname -m)" == "s390x" ]]; then
-        export PATH=$PATH:/usr/local/go/bin
-        export GOROOT=/usr/local/go
-        export GOPATH=/go
-        cd /go/src
         git clone https://github.com/securego/gosec.git
         cd gosec
-        go mod download
-        CGO_ENABLED=0 GOOS=linux GOARCH=s390x go build -ldflags '-w -s' -o gosec ./cmd/gosec/
-        cp gosec /usr/local/bin
-        rm -rf /go/src/gosec
-elif [[ "$(uname -m)" == "x86_64" || "$(uname -m)" == "aarch64" ]];  then
+        GOBIN=/usr/local/ go install ./cmd/gosec
+elif [[ "$(uname -m)" == "x86_64" ]] || [[ "$(uname -m)" == "aarch64" ]]; then
         curl -sfL https://raw.githubusercontent.com/securego/gosec/master/install.sh | sh -s -- -b /usr/local/bin
 fi
