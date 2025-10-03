89558b8efd361545c0e5698fb18101113db97d6e
Dale Haiducek <19750917+dhaiducek@users.noreply.github.com>
2025-10-03 14:00:00 -0400

    fix: set Go dir permissions; tweak: consolidate go version parsing (#154)
    * Consolidate go version parsing
    
    Signed-off-by: Dale Haiducek <19750917+dhaiducek@users.noreply.github.com>
    
    * fix: set permissions for the Go cache
    
    Signed-off-by: Dale Haiducek <19750917+dhaiducek@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Dale Haiducek <19750917+dhaiducek@users.noreply.github.com>

--- build/setup-go.sh
+++ build/setup-go.sh
@@ -12,7 +12,6 @@ elif [[ "$ARCH" == "aarch64" ]]; then
 fi
 
 mkdir -p /go/{bin,pkg,src} /go/pkg/cache /opt/go
-chmod -R a+rwx /go
 cd /opt/go
 wget --progress=dot:mega "https://dl.google.com/go/go${GOVERSION}.${OS}-${ARCH}.tar.gz"
 tar -C /usr/local -xzf "go${GOVERSION}.${OS}-${ARCH}.tar.gz"
