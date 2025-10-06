0597ee00b096301c37d07725a6da089f57b3d0d0
Dale Haiducek <19750917+dhaiducek@users.noreply.github.com>
2025-10-06 16:27:02 -0400

    fix: ensure gomod parses module line
    Signed-off-by: Dale Haiducek <19750917+dhaiducek@users.noreply.github.com>

--- build/setup-clusteradm.sh
+++ build/setup-clusteradm.sh
@@ -6,7 +6,7 @@ module="open-cluster-management.io/clusteradm"
 
 # Get the latest version of clusteradm
 go_mod=$(curl https://raw.githubusercontent.com/open-cluster-management-io/clusteradm/refs/heads/main/go.mod)
-found_module=$(echo "${go_mod}" | awk '/module/ {print $2}')
+found_module=$(echo "${go_mod}" | awk '/^module/ {print $2}')
 
 if [ "${found_module}" != "${module}" ]; then
     echo "WARN: Currently using clusteradm '${module##*/}'. Found new major version of clusteradm '${found_module##*/}'."
--- build/setup-kubebuilder.sh
+++ build/setup-kubebuilder.sh
@@ -6,7 +6,7 @@ module="sigs.k8s.io/kubebuilder/v4"
 
 # Get the latest version of kubebuilder
 go_mod=$(curl https://raw.githubusercontent.com/kubernetes-sigs/kubebuilder/refs/heads/master/go.mod)
-found_module=$(echo "${go_mod}" | awk '/module/ {print $2}')
+found_module=$(echo "${go_mod}" | awk '/^module/ {print $2}')
 
 if [ "${found_module}" != "${module}" ]; then
     echo "WARN: Currently using kubebuilder '${module##*/}'. Found new major version of kubebuilder '${found_module##*/}'."
