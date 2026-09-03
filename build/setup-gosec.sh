27ea1b70813370af612eda4a357bf667d064a007
Dale Haiducek <19750917+dhaiducek@users.noreply.github.com>
2026-09-03 14:19:37 -0400

    chore: update `gosec` to use `go install`
    Signed-off-by: Dale Haiducek <19750917+dhaiducek@users.noreply.github.com>

--- build/setup-gosec.sh
+++ build/setup-gosec.sh
@@ -2,13 +2,15 @@
 
 set -e
 
-arch=$(uname -m)
+module="github.com/securego/gosec/v2"
+
+# Get the latest version of gosec
+go_mod=$(curl https://raw.githubusercontent.com/securego/gosec/refs/heads/master/go.mod)
+found_module=$(echo "${go_mod}" | awk '/^module/ {print $2}')
+
+if [ "${found_module}" != "${module}" ]; then
+    echo "WARN: Currently using gosec '${module##*/}'. Found new major version of gosec '${found_module##*/}'."
+fi
 
 # Install gosec
-if [[ "${arch}" == "x86_64" ]] || [[ "${arch}" == "aarch64" ]]; then
-        curl -sfL https://raw.githubusercontent.com/securego/gosec/master/install.sh | sh -s -- -b /usr/local/bin
-else
-        git clone https://github.com/securego/gosec.git
-        cd gosec
-        GOBIN=/usr/local/bin go install ./cmd/gosec
-fi
+GOBIN=/usr/local/bin go install "${module}/cmd/gosec@latest"
